62b48cda8f366003fb7c7bf181fdcee9a2dbfa34
Pavel Shahoika <pavel11sg@gmail.com>
2025-08-19 19:28:36 +0300

    first commit

---
+++ src/test/java/com/example/tasks/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,10 @@
+package com.example.tasks.orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
